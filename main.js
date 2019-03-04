fa33eb2117c3fa6dc5e3af9690f2ff4f0255a1a9
Denis Spataru <32475875+TVUniquee@users.noreply.github.com>
2019-03-04 08:54:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+class Worker{
+
+	worker(){
+		console.log('Nume');
+		console.log('Prenume');
+		console.log('suma de bani primita pe ora');
+		console.log('numarul de ore lucrate');
+	}
+	move(){
+		console.log('Move');
+	}
+}
+
+/**/
+
+let worker = new worker();
+worker.refill();
+worker.move();
